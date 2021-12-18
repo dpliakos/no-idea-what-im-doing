2062fe936c48e5ef0711ba44ffe98729ac9aba43
Dimitris Pliakos <dimitrispl96@gmail.com>
2021-12-18 15:04:30 +0200

    configure

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h3>Environment: {process.env.REACT_APP_ENVIRONMENT} </h3>
-        <h3>Environment: {process.env.NODE_ENV} </h3>
+        <h3>REACT_APP_ENVIRONMENT: {process.env.REACT_APP_ENVIRONMENT} </h3>
+        <h3>NODE_ENV: {process.env.NODE_ENV} </h3>
+        <h3>REACT_APP_CONFIGURABLE: {process.env.REACT_APP_CONFIGURABLE} </h3>
       </header>
     </div>
   );
